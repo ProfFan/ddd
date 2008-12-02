b8b416bbb59840ec04cc068e89a7572de48b3f8d
pwainwright <pwainwright@1d73a310-8f1f-0410-b01a-dab0232e19a9>
2008-12-02 10:09:46 +0000

    Created a new tag for 3.3.12-rc1

---
+++ tags/DDD_3_3_12_rc1/ddd/jtest/Holiday.java
@@ -0,0 +1,44 @@
+// $Id$ 
+// Java test program: `Holiday' class
+
+// Copyright (C) 1998 Technische Universitaet Braunschweig, Germany.
+// Written by Andreas Zeller <zeller@gnu.org>.
+// 
+// This file is part of DDD.
+// 
+// DDD is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+// 
+// DDD is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public
+// License along with DDD -- see the file COPYING.
+// If not, see <http://www.gnu.org/licenses/>.
+// 
+// DDD is the data display debugger.
+// For details, see the DDD World-Wide-Web page, 
+// `http://www.gnu.org/software/ddd/',
+// or send a mail to the DDD developers <ddd@gnu.org>.
+
+//--------------------------------------------------------------------------
+// This program defines some data structures and values that may be
+// examined using DDD.
+//--------------------------------------------------------------------------
+
+class Holiday extends Date {
+    private String name;
+
+    public Holiday(int day, int month, int year, String name)
+    {
+	super(day, month, year);
+	this.name = name;
+    }
+
+    public void print() {}
+    public void print(int x) {}
+}
